9370df1f092cc041c29c4d0ce70085eb396e879c
alok <alok.kr300@gmail.com>
2024-09-26 11:05:59 +0530

    getting data from the PRODUCTS model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,19 @@
 import json
 from django.http import JsonResponse
 
+from products.models import Product
 
 def api_home(request,*args,**kwargs):
 
-    body = request.body # byte string of JSON DATA
+    # Product.object.all().order_by("?") : This creates random query set 
+    # and .first() gets the first value
+    model_data = Product.objects.all().order_by("?").first()
+
     data = {}
 
-    try:
-        data = json.loads(body) # String of JSON data to python Dictionary
-    except:
-        pass
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
 
-    print(request.GET) # url query params
-    print(data)
-    return JsonResponse({"message":"HI, There this is JSON Response"})
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,4 +4,4 @@ endpoint  = "http://127.0.0.1:8000/api/"
 
 op = requests.get(endpoint,params={"abc":123},json={"query":"Hello"})
 
-print(op.text)
+print(op.json())

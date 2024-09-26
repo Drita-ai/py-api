767e5f78e13ec654349cdbbe1d61c6586dcd5484
alok <alok.kr300@gmail.com>
2024-09-26 16:08:32 +0530

    implemented basic serialization

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,17 +3,20 @@ from django.http import JsonResponse
 from django.forms.models import model_to_dict
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 def api_home(request,*args,**kwargs):
 
     # Product.object.all().order_by("?") : This creates random query set 
     # and .first() gets the first value
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
 
     data = {}
 
-    if model_data:
+    if instance:
         # fields are the data that i need to add only
-        data = model_to_dict(model_data , fields=['id','title','price'])
+        # data = model_to_dict(instance , fields=['id','title','price','sale_price','get_discount'])
+
+        data = ProductSerializer(instance).data
 
     return JsonResponse(data)
